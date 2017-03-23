4045ade5962486b67f699d10a4eb675a7f683725
Julia Kieserman <jk790u@att.com>
2017-03-23 11:29:15 -0500

    add email service and dependencies

---
+++ src/app/services/email/email.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { AngularFire } from 'angularfire2';
+import { Md5 } from 'ts-md5/dist/md5';
+import { SubscriptionObj } from '../../models/subscription-model';
+
+@Injectable()
+export class EmailService {
+
+  private _af;
+
+  constructor(af: AngularFire) { 
+    this._af = af;
+  }
+
+  public addEmail(email: SubscriptionObj, hash: any) {
+    const databaseObj = this._af.database.object('/emails');
+    databaseObj.update({ [hash]: email });
+  }
+
+}
