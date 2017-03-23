2c04f7ccff2766d66926ce1e04b6aef40b279f62
Julia Kieserman <jk790u@att.com>
2017-03-23 10:53:09 -0500

    import new modules and set routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,19 @@ import { ClarityModule } from 'clarity-angular';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
+export const firebaseConfig = {
+    apiKey: "AIzaSyAq3BR1axTBqeqdqHWbqF68bPShUOiML8Y",
+    authDomain: "jokes-website.firebaseapp.com",
+    databaseURL: "https://jokes-website.firebaseio.com",
+    storageBucket: "jokes-website.appspot.com",
+    messagingSenderId: "1016586563889"
+};
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +30,10 @@ import { HomePageComponent } from './home-page/home-page.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    ClarityModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
