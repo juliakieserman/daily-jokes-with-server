9f4a267f835bc323f8057084f91c8307d0de23c5
Julia Kieserman <jbk67@georgetown.edu>
2017-04-04 06:19:35 -0500

    confirmed this works with sandbox on local, need to find heroku issue

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ var app = express();
 const nodemailer = require('nodemailer');
 const mg = require('nodemailer-mailgun-transport');
 
+/* sandbox credentials: 
+const auth = {
+  auth: {
+    api_key: 'key-aa1fecf5c0fa298f77f60b63b76a9768',
+    domain: 'sandboxd7ba3dbd75e8434990cdd9d7e7346e96.mailgun.org'
+  }
+}*/
+
+/* mailgun heroku credentials */
 const auth = {
   auth: {
     api_key: process.env.MAILGUN_API_KEY,
@@ -30,19 +39,18 @@ app.use(express.static(__dirname + '/dist'));
 app.use('api', api);
 
 console.log('sending mail');
-var transporter = Email.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    auth: {
-        api_key: process.env.MAILGUN_API_KEY,
-        domain: process.env.MAILGUN_DOMAIN
-    }
-});
-transporter.sendMail({
+
+nodemailerMailgun.sendMail({
     from: 'kieserman.julia@gmail.com',
     to: 'kieserman.julia@gmail.com',
     subject: 'test nodemailer',
     text: 'test works!'
+}, function(err, info) {
+    if (err) {
+        console.log('Error: ' + err);
+    } else {
+        console.log('Response: ' + info);
+    }
 });
 
 //let angular handle the routing
