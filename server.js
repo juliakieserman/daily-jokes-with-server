74a5fb17ad570fe859863c5ac522000208016f78
Julia Kieserman <jbk67@georgetown.edu>
2017-04-10 14:22:47 -0400

    trying to fix routing

--- server.js
+++ server.js
@@ -10,13 +10,17 @@ app.use('api', api);
 /* end server set-up */
 
 //let angular handle the routing
-app.get('/', function(req, res) {
+app.all('/*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('index.html', { root: __dirname });
+})
+/*app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.get('/home', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+});*/
 
 app.listen(process.env.PORT || 8080, function() {
     console.log('listening');
