4286350ccadf6ea78b0296bc71e0e28fe70b3887
Julia Kieserman <jk790u@att.com>
2017-04-19 13:37:12 -0500

    add day count on about page with animation

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, keyframes, transition, animate } from '@angular/animations';
 import { Router } from '@angular/router';
 import { JokeService } from '../services/jokes/joke.service';
 
@@ -6,17 +7,47 @@ import { JokeService } from '../services/jokes/joke.service';
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
   styleUrls: ['./about-page.component.css'],
+  animations: [
+    trigger('flip', [
+      transition('inactive => active', [
+        animate(1000, keyframes([
+          style({transform: 'perspective(400px) rotate3d(1, 0, 0, 90deg)', offset: .15}),
+          style({transform: 'perspective(400px) rotate3d(1, 0, 0, -20deg)', offset: .40}),
+          style({transform: 'perspective(400px) rotate3d(1, 0, 0, 10deg)', offset: .60}),
+          style({transform: 'perspective(400px) rotate3d(1, 0, 0, -5deg)', offset: .80}),
+          style({transform: 'perspective(400px)', offset: 1})
+        ]))
+      ])
+    ])
+  ],
   providers: [JokeService]
 })
 export class AboutPageComponent implements OnInit {
 
+  flipState: string = 'inactive';
+  content: string = '?';
+  reveal: string = 'click to reveal';
+  jokeCount: number;
+
   constructor(
     private jokeService: JokeService,
     private router: Router) {
-    
    }
 
   ngOnInit() {
+    this.getJokeCount();
+  }
+
+  toggleMove() {
+    this.flipState = 'active';
+    this.content = this.jokeCount.toString();
+    this.reveal = 'days and counting';
+  }
+
+  private getJokeCount() {
+    this.jokeService.getDailyJokeCount().subscribe(joke => {
+      this.jokeCount = joke[0].count;
+    }) 
   }
 
   private randomJoke() {
