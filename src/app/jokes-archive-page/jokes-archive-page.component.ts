51b4354de4f004a2f4fd8f6729c9ab1dd4e350cc
Julia Kieserman <jbk67@georgetown.edu>
2017-04-25 11:47:51 -0500

    add a little shake to the archives

--- src/app/jokes-archive-page/jokes-archive-page.component.ts
+++ src/app/jokes-archive-page/jokes-archive-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
+import { trigger, state, style, keyframes, transition, animate } from '@angular/animations';
 import { Router } from '@angular/router';
 import { AngularFire } from 'angularfire2';
 import { JokeObj } from '../models/joke-model';
@@ -7,7 +8,19 @@ import { JokeService } from '../services/jokes/joke.service';
 @Component({
   selector: 'app-jokes-archive-page',
   templateUrl: './jokes-archive-page.component.html',
-  styleUrls: ['./jokes-archive-page.component.css']
+  styleUrls: ['./jokes-archive-page.component.css'],
+  animations: [
+    trigger('shake', [
+      transition('inactive => active', [
+        animate(1000, keyframes([
+          style({transform: 'translate3d(-1px, 0, 0)', offset: .1}),
+          style({transform: 'translate3d(2px, 0, 0)', offset: .2}),
+          style({transform: 'translate3d(-4px, 0, 0)', offset: .3}),
+          style({transform: 'translate3d(4px, 0, 0)', offset: .4})
+        ]))
+      ])
+    ])
+  ]
 })
 export class JokesArchivePageComponent implements OnInit {
 
@@ -18,6 +31,8 @@ export class JokesArchivePageComponent implements OnInit {
   private searchText = '';
   private titles: JokeObj[] = [];
 
+  private shakeState: string = 'inactive';
+
   constructor(af: AngularFire, private router: Router, private element: ElementRef) {
     this._af = af;
     this.elementRef = element;
@@ -26,6 +41,16 @@ export class JokesArchivePageComponent implements OnInit {
   ngOnInit() {
     this.jokeList = [];
     this.loadData();
+  }
+
+  toggleShake() {
+    if (this.shakeState == 'active') {
+      console.log('setting to inactive');
+      this.shakeState = 'inactive';
+    } else {
+      console.log('setting to active');
+      this.shakeState = 'active';
+    }
   }
 
   private loadData() {
